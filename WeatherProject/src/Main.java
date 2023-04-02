54d6990c9609938798175416ddf202119d4e7613
Vyacheslav <112700264+rel1se@users.noreply.github.com>
2023-04-02 11:52:53 +0300

    Add files via upload

---
+++ WeatherProject/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        PascalCounter pascalCounter = new PascalCounter();
+        System.out.println(pascalCounter.circulationIndexCounter());
+        System.out.println(pascalCounter.classificationOfDays());
+    }
+}
